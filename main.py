d0c299a74c8c7e5ad72dab391fe47f39021439e6
CoolShablul <Wooowy@gmail.com>
2022-12-04 21:34:06 +0200

    eii eiii done - requires checking :smoothing + pseudo Data (including the unknown words case). in addition needs to implement confusion matrix

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import numpy as np
+import regex
 from nltk.corpus import brown
 from operator import itemgetter
+import nltk
 import json
 import re
+
+NUMER_REGEX = r"\d+"
+
+MONEY_REGEX = r"\d+[€$¥₪₹]"
+
+TIME_REGEX = r"^(?:[01]?\d|2[0-3])(?::[0-5]\d){1,2}$"
+
+DATE_REGEX = r"\d{1,2}-\d{1,2}-\d{2,4}|\d{1,2}\/\d{1,2}\/\d{2,4}|\d{1,2}\.\d{1,2}\.\d{2,4}"
+
+SPARSE_WORD_THRESHOLD = 5
+
+nltk.download('brown')
 
 FILTER_COMPLEX_TAG = re.compile(r"[^*+\-]*")
 
@@ -21,10 +36,13 @@ def split_train_test():
 class Bigram_HMM:
     def __init__(self, training, test, smoothing=False):
         self.known_words = set()
+        self.unknown_words = set()
         self.train_emission = self.init_emission(training)
         self.train_transition = self.init_transition(training)
         self.all_w = self.known_words.copy()
         self.smoothing = smoothing
+        self.posTagger = posTagger(train, test)
+
         if self.smoothing:
             self.add_test(test)
             self.smooth()
@@ -109,6 +127,7 @@ class Bigram_HMM:
             if i == 0:
                 viterbi_matrix[i][0] = (1.0, ["START"])
             elif not self.check_known_word(w):  # unknown word
+                self.unknown_words.add(w)
                 p, tags = self.calc_prev_max_tag(all_tags, viterbi_matrix, i - 1, "NN")
                 viterbi_matrix[i][3] = (p, tags)
             else:  # known word
@@ -124,18 +143,29 @@ class Bigram_HMM:
         predicted_tags = max(viterbi_matrix[k - 1], key=lambda item: item[0])
         return predicted_tags[1][1:]
 
-    def viterbi_error(self, data):
+    def viterbi_error(self, data, realData = None):
         test_size = 0
         correct = 0
         unknown_correct = 0
         unknown_words = 0
         known_words = 0
-        for sentence in data:
+        for j, sentence in enumerate(data):
             sent, tags = [list(i) for i in zip(*sentence)]
             tags = [filter_tag(t) for t in tags]
             predicted_tags = self.calc_Viterbi(sent)
             for i, word in enumerate(sent):
-                if not self.check_known_word(word):
+
+                if realData:  #case of pseudo words - we need to check if a word is unknown or not with the real data instead of pseudo data
+                    if not (self.check_known_word(realData[j][i][0])):
+                        unknown_words += 1
+                        if tags[i] == predicted_tags[i]:  # correct guess for unknown word
+                            unknown_correct += 1
+                    else:
+                        known_words += 1
+                        if tags[i] == predicted_tags[i]:
+                            correct += 1
+
+                elif not self.check_known_word(word):
                     unknown_words += 1
                     if tags[i] == predicted_tags[i]:  # correct guess for unknown word
                         unknown_correct += 1
@@ -239,6 +269,128 @@ class posTagger:
         return 1 - accuracy
 
 
+class pseudoTagger:
+    def __init__(self, training, test):
+        self.bigHMM = Bigram_HMM(training, test)
+        self.trainData = training
+        self.testData = test
+        self.pseudoTrainingData = []
+        self.numOfUnknown = 0
+        # self.pseudoDict = dict()
+        self.pseudoWords = set()
+        self.pseudoTestData = []
+        # self.pseudoDict =
+
+
+    def get_word_count(self, word):
+        """
+        given a word - counts how many times it apears in the training data according to the word tags dict
+        :param word:
+        :return:
+        """
+        count = sum(self.bigHMM.posTagger.train_words_tags[word].values())
+        return count
+
+    def getPseudoWord(self, word):
+        """
+        given a sparse word categorise it into a pseudo category and return the category along with its original tag
+        :param word:
+        :return: pseudo word and original tag
+        """
+
+        if word.isupper() and not word == word.capitalize():                #capitalised word - name / term
+            return "PWcapName"
+
+        if word == word.capitalize() and not word.isnumeric():     #all caps
+            return "PWallCaps"
+
+        if re.compile(DATE_REGEX).fullmatch(word):      #date
+            return "PWdate"
+
+        if re.compile(TIME_REGEX).fullmatch(word):            #time with :
+            return "PWtime"
+
+        if re.compile(NUMER_REGEX).fullmatch(word):              #just a number
+            return "PWnumber"
+
+        if re.compile(MONEY_REGEX).fullmatch(word):     #money
+            return "PWmoney"
+
+        if '-' in word:                                     #has dash
+            return "PWdash"
+
+        if '/' in word:                                     #has slash
+            return "PWslash"
+
+        return "PWother"                                      #uncategorised
+
+
+
+    def getPseudoDataTrain(self):
+        """
+        creates a new data that replaces sparse words with pseudo words along with matching psuedo words-tags dictionary
+        :param data: train data
+        :return:   pseudoDict: words-tag dictionary which replaces sparse words with pseudo words
+                   pseudoData: tagged text data which replaces sparse words with pseudo words with the original tag
+        """
+        pseudoDict = dict()
+        pseudoData = []
+        for sent in self.trainData:
+            pseudoSent = []
+            for word, tag in sent:
+                if self.get_word_count(word) <= SPARSE_WORD_THRESHOLD:            #sparse word - turn into a pseudo word
+                    self.pseudoWords.add(word)
+                    pseudoWord = self.getPseudoWord(word)
+                    pseudoSent.append((pseudoWord, tag))
+
+                    pDict = pseudoDict.setdefault(pseudoWord, dict())       #create a dictionary for a pseudoword
+                    pDict[pseudoWord] = pDict.get(tag, 0) + 1
+
+                else:
+                    pseudoSent.append((word, tag))
+
+            pseudoData.append(pseudoSent)  # add sentence with pseudo words
+
+        self.pseudoTrainingData = pseudoData
+        # self.pseudoDict = pseudoDict
+
+
+    def getPseudoDataTest(self):
+        """
+        creates a new data that replaces sparse words and unknown words with pseudo words along with matching psuedo
+        words-tags dictionary
+        :param data: test data
+        :return:   pseudoDict: words-tag dictionary which replaces sparse words with pseudo words
+                   pseudoData: tagged text data which replaces sparse words with pseudo words with the original tag
+        """
+        pseudoData = []
+        for sent in self.testData:
+            pseudoSent = []
+            for word, tag in sent:
+                if not(self.bigHMM.check_known_word(word)):   #unknown word - insructed to replace with pseudo word
+                    self.numOfUnknown += 1
+                    pseudoWord = self.getPseudoWord(word)
+                    pseudoSent.append((pseudoWord, tag))
+
+                elif word in self.pseudoWords:              #was replaced by a pseudo word in training and therefore needs to be replaced by pseudo in test as well
+                    pseudoWord = self.getPseudoWord(word)
+                    pseudoSent.append((pseudoWord, tag))
+
+                else:
+                    pseudoSent.append((word, tag))
+
+            pseudoData.append(pseudoSent)  # add sentence with pseudo words
+
+        self.pseudoTestData = pseudoData
+
+    def retrainWithPseudo(self, smoothing=False):
+        """
+        train the bigram and posTagger with the pseudo data
+        :return:
+        """
+        self.bigHMM = Bigram_HMM(self.pseudoTrainingData, self.pseudoTestData, smoothing)
+
+
 def B():
     train, test = split_train_test()
     p = posTagger(train, test)
@@ -254,9 +406,15 @@ def D(train, test):
     hmm = Bigram_HMM(train, test, True)
     hmm.viterbi_error(test)
 
+def E(train, test):
+    pt = pseudoTagger(train, test)
+    pt.getPseudoDataTrain()
+    pt.getPseudoDataTest()
+    # pt.retrainWithPseudo()
+    # pt.bigHMM.viterbi_error(pt.pseudoTestData, pt.testData)
+    pt.retrainWithPseudo(True)
+    pt.bigHMM.viterbi_error(pt.pseudoTestData, pt.testData)
 
 if __name__ == '__main__':
     train, test = split_train_test()
-    C(train, test)
-    print("now Q---d")
-    D(train, test)
+    E(train, test)
