d6134658926c5c9b7c5eefa21526ceacf57c34d0
Amir3158 <amir.kaya7@gmail.com>
2022-12-04 16:09:53 +0200

    finished C and D

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import numpy as np
 from nltk.corpus import brown
 from operator import itemgetter
-import nltk
 import json
 import re
-
-nltk.download('brown')
 
 FILTER_COMPLEX_TAG = re.compile(r"[^*+\-]*")
 
@@ -23,9 +19,15 @@ def split_train_test():
 
 
 class Bigram_HMM:
-    def __init__(self, training, test):
-        self.pos_tagger = posTagger(training, test)
+    def __init__(self, training, test, smoothing=False):
+        self.known_words = set()
+        self.train_emission = self.init_emission(training)
         self.train_transition = self.init_transition(training)
+        self.all_w = self.known_words.copy()
+        self.smoothing = smoothing
+        if self.smoothing:
+            self.add_test(test)
+            self.smooth()
 
     def init_transition(self, data):
         transition = dict()
@@ -36,9 +38,39 @@ class Bigram_HMM:
                 inner = transition.setdefault(last_tag, dict())
                 inner[t] = inner.get(t, 0) + 1
                 last_tag = t
+                self.known_words.add(w)
             inner = transition.setdefault(last_tag, dict())
             inner["STOP"] = inner.get("STOP", 0) + 1
         return transition
+
+    def init_emission(self, data):
+        words_tag = dict()
+        for sent in data:
+            for word in sent:
+                w_tag = filter_tag(word[1])
+                if word[0] in words_tag:
+                    if w_tag in words_tag[word[0]]:
+                        words_tag[word[0]][w_tag] += 1
+                    else:
+                        words_tag[word[0]][w_tag] = 1
+                else:
+                    words_tag[word[0]] = dict()
+                    words_tag[word[0]][w_tag] = 1
+        return words_tag
+
+    def add_test(self, test):
+        for sentence in test:
+            for word, tag in sentence:
+                self.all_w.add(word)
+
+    def smooth(self):
+        for w in self.all_w:
+            inner = self.train_emission.setdefault(w, dict())
+            for t in self.train_transition.keys():
+                inner[t] = inner.get(t, 0) + 1
+
+    def check_known_word(self, word):
+        return word in self.known_words
 
     def calc_transition(self, tag1, tag2):
         if tag1 not in self.train_transition:
@@ -47,10 +79,11 @@ class Bigram_HMM:
         return count_tag1_tag2 / sum(self.train_transition[tag1].values())
 
     def calc_emission(self, t, w):
-        c = self.pos_tagger.get_words_tag_count(w, t)  # amount of occurrences the word w is tagged to t
-        if c == -1:
+        if not self.check_known_word(w):
             return -1
-        return c / sum(self.train_transition[t].values())
+        c = self.train_emission[w].get(t, 0)  # amount of occurrences the word w is tagged to t
+        d = len(self.all_w) if self.smoothing else 0
+        return c / (sum(self.train_transition[t].values()) + d)
 
     def calc_prev_max_tag(self, all_tags, matrix, i, tag):
         max_prob = 0
@@ -73,15 +106,13 @@ class Bigram_HMM:
         text.extend(sent)
         for i, w in enumerate(text):
             viterbi_matrix.append([(0, [])] * num_of_tags)
-            if i > 0 and not self.pos_tagger.check_known_word(w):  # unknown word
+            if i == 0:
+                viterbi_matrix[i][0] = (1.0, ["START"])
+            elif not self.check_known_word(w):  # unknown word
                 p, tags = self.calc_prev_max_tag(all_tags, viterbi_matrix, i - 1, "NN")
-                viterbi_matrix[i][1] = (p, tags)
+                viterbi_matrix[i][3] = (p, tags)
             else:  # known word
                 for j, t in enumerate(all_tags):
-                    if i == 0:
-                        if t == "START":
-                            viterbi_matrix[i][j] = (1.0, ["START"])
-                        continue
                     e = self.calc_emission(t, w)
                     if e == 0: continue
                     p, tags = self.calc_prev_max_tag(all_tags, viterbi_matrix, i - 1, t)
@@ -91,7 +122,6 @@ class Bigram_HMM:
                 tags.extend(["NN"] * (k - i))
                 return tags[1:]
         predicted_tags = max(viterbi_matrix[k - 1], key=lambda item: item[0])
-        print(1)
         return predicted_tags[1][1:]
 
     def viterbi_error(self, data):
@@ -105,7 +135,7 @@ class Bigram_HMM:
             tags = [filter_tag(t) for t in tags]
             predicted_tags = self.calc_Viterbi(sent)
             for i, word in enumerate(sent):
-                if not self.pos_tagger.check_known_word(word):
+                if not self.check_known_word(word):
                     unknown_words += 1
                     if tags[i] == predicted_tags[i]:  # correct guess for unknown word
                         unknown_correct += 1
@@ -125,10 +155,13 @@ class Bigram_HMM:
 
 class posTagger:
 
-    def __init__(self, training, test, file=None):
+    def __init__(self, training, test, smoothing=False, file=None):
         self.train_words_tags = self.init_word_tags(training)
         self.test_words_tags = self.init_word_tags(test)
         self.train_words_mle = self.init_mle(self.train_words_tags) if file is None else self.get_mle_from_json(file)
+
+    def smoothing(self):
+        pass
 
     def get_words_tag_count(self, word, tag):
         if not self.check_known_word(word):
@@ -136,9 +169,10 @@ class posTagger:
         return self.train_words_tags[word].get(tag, 0)
 
     def check_known_word(self, word):
-        if word not in self.train_words_tags:
-            return False
-        return True
+        return word in self.train_words_tags
+        # if word not in self.train_words_tags:
+        #     return False
+        # return True
 
     def init_word_tags(self, data):
         """
@@ -211,13 +245,18 @@ def B():
     p.mle_error()
 
 
-def C():
-    train, test = split_train_test()
+def C(train, test):
     hmm = Bigram_HMM(train, test)
     hmm.viterbi_error(test)
 
 
+def D(train, test):
+    hmm = Bigram_HMM(train, test, True)
+    hmm.viterbi_error(test)
 
 
 if __name__ == '__main__':
-    C()
+    train, test = split_train_test()
+    C(train, test)
+    print("now Q---d")
+    D(train, test)
